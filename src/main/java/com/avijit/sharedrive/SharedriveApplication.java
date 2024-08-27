1a4af837ceab6246532214476588a4d1524dae81
Avijit Maity <avisrabon22@gmail.com>
2024-08-28 00:54:10 +0530

    first commit

---
+++ src/main/java/com/avijit/sharedrive/SharedriveApplication.java
@@ -0,0 +1,13 @@
+package com.avijit.sharedrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avijit/sharedrive/SharedriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avijit.sharedrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
